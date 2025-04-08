5a8aaf293f55612bae6bad4125197bfc51c69c8c
Umar Al Mukhtar <umar1868807@gmail.com>
2025-04-08 07:35:29 +0530

    add BasiCalator.dart

---
+++ BasiCulator.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+void main() {
+  print("📌 BasiCulator - Basic Calculator");
+  print("Enter the first number: ");
+  double? n1 = double.parse(stdin.readLineSync()!);
+  print("Enter the second number: ");
+  double? n2 = double.parse(stdin.readLineSync()!);
+  print("Choose an operation (+, -, *, /): ");
+  String? operation = stdin.readLineSync();
+  double result = 0.0;
+  switch (operation) {
+    case '+':
+      result = n1 + n2;
+      break;
+    case '-':
+      result = n1 - n2;
+      break;
+    case '*':
+      result = n1 * n2;
+      break;
+    case '/':
+      if (n2 != 0) {
+        result = n1 / n2;
+      } else {
+        print("Error: Division by zero is not allowed.");
+        return;
+      }
+      break;
+    default:
+      print("Error: Invalid operation.");
+      return;
+  }
+  print("The result of $n1 $operation $n2 is $result");
+}
